ac9ad1db53f7a2633f7d0ef9a5e057d8bf82cd7f
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-26 18:59:25 +0200

    add loaders

---
+++ src/styles/components/loaders/MiniSpinner.tsx
@@ -0,0 +1,12 @@
+
+
+export default function MiniSpinner() {
+return (
+<>
+<div className='mini-spinner'>
+<svg viewBox="25 25 50 50">
+  <circle r="20" cy="50" cx="50"></circle>
+</svg></div>
+</>
+)
+}
